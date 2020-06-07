8d119f2fc0ac2c48be8726f190465efad4230d3d
ChangWenhan <53159178+ChangWenhan@users.noreply.github.com>
2020-06-07 15:22:29 +0800

    Add files via upload

---
+++ 计算机图形学期末作业-第一题.cpp
@@ -0,0 +1,126 @@
+﻿#include<glut.h>
+#include<gl/GL.h>
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+void init(void)
+{
+    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glMatrixMode(GL_PROJECTION);
+    gluOrtho2D(0.0, 200.0, 0.0, 150.0);
+}
+
+/*
+    画点
+*/
+void setPixel(int x, int y)
+{
+    glColor3f(0.0, 0.0, 1.0);       // 蓝色
+    glPointSize(2.0f);
+    glBegin(GL_POINTS);
+    glVertex2f(x, y);
+    glEnd();
+    glFlush();
+}
+
+/*
+    椭圆4路对称
+*/
+void Ellipsepot(int x0, int y0, int x, int y)
+{
+
+    setPixel((x0 + x), (y0 + y));
+    setPixel((x0 + x), (y0 - y));
+    setPixel((x0 - x), (y0 - y));
+    setPixel((x0 - x), (y0 + y));
+}
+
+/*
+    中点画椭圆算法
+*/
+void MidPoint_Ellipse(int x0, int y0, int a, int b)
+{
+    double sqa = a * a;
+    double sqb = b * b;
+
+    double d = sqb + sqa * (0.25 - b);
+    int x = 0;
+    int y = b;
+    Ellipsepot(x0, y0, x, y);
+    // 1
+    while (sqb * (x + 1) < sqa * (y - 0.5))
+    {
+        if (d < 0)
+        {
+            d += sqb * (2 * x + 3);
+        }
+        else
+        {
+            d += (sqb * (2 * x + 3) + sqa * ((-2) * y + 2));
+            --y;
+        }
+        ++x;
+        Ellipsepot(x0, y0, x, y);
+    }
+    d = (b * (x + 0.5)) * 2 + (a * (y - 1)) * 2 - (a * b) * 2;
+    // 2
+    while (y > 0)
+    {
+        if (d < 0)
+        {
+            d += sqb * (2 * x + 2) + sqa * ((-2) * y + 3);
+            ++x;
+        }
+        else
+        {
+            d += sqa * ((-2) * y + 3);
+        }
+        --y;
+        Ellipsepot(x0, y0, x, y);
+    }
+}
+
+// 窗口大小改变时调用的登记函数
+void ChangeSize(GLsizei w, GLsizei h)
+{
+
+    if (h == 0)     h = 1;
+
+    // 设置视区尺寸
+    glViewport(0, 0, w, h);
+
+    // 重置坐标系统
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    // 建立修剪空间的范围
+    if (w <= h)
+        glOrtho(0.0f, 250.0f, 0.0f, 250.0f * h / w, 1.0, -1.0);
+    else
+        glOrtho(0.0f, 250.0f * w / h, 0.0f, 250.0f, 1.0, -1.0);
+
+}
+
+void display(void)
+{
+    // 用当前背景色填充窗口，如果不写这句会残留之前的图像
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    int x0 = 120, y0 = 100, a = 100, b = 60;
+    MidPoint_Ellipse(x0, y0, a, b);
+}
+
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+    glutInitWindowPosition(200, 200);
+    glutInitWindowSize(400, 400);
+    glutCreateWindow("椭圆形");
+    glutDisplayFunc(display);
+    glutReshapeFunc(ChangeSize);
+    init();
+    glutMainLoop();
+    return 0;
+}
---
+++ 计算机图形学期末作业-第三题.cpp
@@ -0,0 +1,150 @@
+﻿// Bounce.cpp
+// Demonstrates a simple animated rectangle program with GLUT
+//用GLUT演示一个简单的动画矩形程序
+// OpenGL SuperBible, 3rd Edition
+
+#include <glut.h>	// OpenGL toolkit
+#include<gl/GL.h>
+#include<gl/GLU.h>
+
+
+// 初始化矩形位置和大小
+GLfloat x = 0.0f;
+GLfloat y = 0.0f;
+GLfloat rsize = 25;
+
+// x和y方向上的步长
+// (每次移动的像素)
+GLfloat xstep = 1.0f;
+GLfloat ystep = 1.0f;
+
+// 跟随窗口改变长宽
+GLfloat windowWidth;
+GLfloat windowHeight;
+
+///////////////////////////////////////////////////////////
+// 绘画场景
+void RenderScene(void)
+{
+    // 使用当前的清除颜色清除窗口
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // 设置小方块颜色为红色
+    glColor3f(1.0f, 0.0f, 0.0f);
+
+    // 用当前颜色绘制一个填充的矩形
+    glRectf(x, y, x + rsize, y - rsize);
+
+    // Flush drawing commands and swap
+    //刷新绘图命令并交换
+    glutSwapBuffers();
+}
+
+///////////////////////////////////////////////////////////
+// 空闲时由GLUT库调用（不调整窗口大小或移动窗口）
+void TimerFunction(int value)
+{
+    // Reverse direction when you reach left or right edge
+    //到达左边缘或右边缘时方向相反--用于反弹效果
+    if (x > windowWidth - rsize || x < -windowWidth)
+        xstep = -xstep;
+
+    // Reverse direction when you reach top or bottom edge
+    //到达上边缘或下边缘时方向相反--用于反弹效果
+    if (y > windowHeight || y < -windowHeight + rsize)
+        ystep = -ystep;
+
+    // Actually move the square
+    //移动矩形
+    x += xstep;
+    y += ystep;
+
+    // Check bounds. This is in case the window is made
+    // smaller while the rectangle is bouncing and the 
+    // rectangle suddenly finds itself outside the new
+    // clipping volume
+    //检查边界，这是在窗口被建立的更小
+    //当长方形在弹跳并且长方形忽然发现他跑到了新剪裁量外
+    if (x > (windowWidth - rsize + xstep))
+        x = windowWidth - rsize - 1;
+    else if (x < -(windowWidth + xstep))
+        x = -windowWidth - 1;
+
+    if (y > (windowHeight + ystep))
+        y = windowHeight - 1;
+    else if (y < -(windowHeight - rsize + ystep))
+        y = -windowHeight + rsize - 1;
+
+    // Redraw the scene with new coordinates
+    glutPostRedisplay();
+    glutTimerFunc(33, TimerFunction, 1);    //需要在函数中再调用一次，才能保证循环
+}
+
+///////////////////////////////////////////////////////////
+// Setup the rendering state
+void SetupRC(void)
+{
+    // Set clear color to blue
+    glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+}
+
+///////////////////////////////////////////////////////////
+// Called by GLUT library when the window has chanaged size
+//修改窗口大小
+void ChangeSize(int w, int h)
+{
+    GLfloat aspectRatio;
+
+    // Prevent a divide by zero
+    if (h == 0)
+        h = 1;
+
+    // Set Viewport to window dimensions
+    //将视口设置为窗口尺寸
+    glViewport(0, 0, w, h);
+
+    // Reset coordinate system
+    //重置坐标系
+    glMatrixMode(GL_PROJECTION);    //GL_PROJECTION--投影模式
+    glLoadIdentity();      //将当前点移到了屏幕中心：类似于一个复位操作
+
+    // Establish clipping volume (left, right, bottom, top, near, far)
+    aspectRatio = (GLfloat)w / (GLfloat)h;
+
+    //glOrtho(left, right, bottom, top, near, far)
+    //left表示视景体左面的坐标, right表示右面的坐标, bottom表示下面的, top表示上面的
+    if (w <= h)
+    {
+        windowWidth = 100;
+        windowHeight = 100 / aspectRatio;
+        glOrtho(-100.0, 100.0, -windowHeight, windowHeight, 1.0, -1.0);
+    }
+    else
+    {
+        windowWidth = 100 * aspectRatio;
+        windowHeight = 100;
+        glOrtho(-windowWidth, windowWidth, -100.0, 100.0, 1.0, -1.0);
+    }
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+///////////////////////////////////////////////////////////
+// Main program entry point
+int main(int argc, char* argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Bounce");
+    glutDisplayFunc(RenderScene);   //设置绘画场景
+    glutReshapeFunc(ChangeSize);    //窗口大小改变时调用
+    glutTimerFunc(33, TimerFunction, 1);    //定时器，33毫秒，回调函数指针TimerFunction,区别值
+
+    SetupRC();  //Set clear color to blue
+
+    glutMainLoop();     // 让主事件无限循环
+
+    return 0;
+}
